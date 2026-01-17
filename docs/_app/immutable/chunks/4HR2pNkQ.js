ff299b786212d002fa61fdcbcb6fd523a5429392
Sahil Ramjeeth <22046468@dut4life.ac.za>
2026-01-17 14:25:14 +0200

    Fix D

---
+++ docs/_app/immutable/chunks/4HR2pNkQ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_lyx50f?.base??"/axelbase-hybrid-scanner",a=globalThis.__sveltekit_lyx50f?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/BjOh7rY1.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_j1t991?.base??"/axelbase-hybrid-scanner",a=globalThis.__sveltekit_j1t991?.assets??s??"";export{a,s as b};
